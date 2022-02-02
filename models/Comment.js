da46373854ac40a7960ab78c178ffcd051f35f91
JonelleH <jonelle.harris04@gmail.com>
2022-02-02 18:00:46 -0500

    updated folder

--- models/Comment.js
+++ models/Comment.js
@@ -2,14 +2,17 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Comment extends Model {}
-//COMMENT  MODEL
+
 Comment.init(
   {
     id: {
-      type: DataTypes.INTEGER,
+      
+        type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
-    },
+   
+     },
+    
     comment_text: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -17,29 +20,35 @@ Comment.init(
         len: [1, 150],
       },
     },
+    
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: "user",
         key: "id",
-      },
+      
+        },
     },
+    
     post_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: "post",
         key: "id",
-      },
+     
+        },
     },
   },
   {
+    
     sequelize,
     freezeTableName: true,
     underscored: true,
     modelName: "comment",
-  }
+  
+    }
 );
 
 module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -5,29 +5,38 @@ class Post extends Model {}
 
 Post.init(
   {
+    
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+      
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
+   
     title: {
+      
       type: DataTypes.STRING,
       allowNull: false,
     },
+    
     content: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      
+        type: DataTypes.STRING,
+        allowNull: false,
     },
+    
     user_id: {
-      type: DataTypes.INTEGER,
-      references: {
+      
+        type: DataTypes.INTEGER,
+         references: {
         model: "user",
         key: "id",
       },
     },
   },
   {
+    
     sequelize,
     freezeTableName: true,
     underscored: true,
