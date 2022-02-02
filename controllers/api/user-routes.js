8aeda905da2e7a90c442650786c2ba49c31ad4eb
JonelleH <jonelle.harris04@gmail.com>
2022-02-02 18:20:01 -0500

    added code

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,194 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", (req, res) => {
+  
+    User.findAll()
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  
+  User.findOne({
+    
+    where: {
+      
+      id: req.params.id,
+    },
+    
+    attributes: { exclude: ["password"] },
+    include: [
+      {
+        
+        model: Post,
+        attributes: ["id", "title", "created_at"],
+      },
+      {
+        
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          
+          model: Post,
+          attributes: ["title"],
+        },
+      },
+    ],
+  })
+    
+    .then((dbUserData) => {
+      
+      if (!dbUserData) {
+        
+        res.status(404).json({ message: "USER NOT FOUND" });
+        return;
+      }
+      
+      res.json(dbUserData);
+    })
+    
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  
+  User.create(req.body)
+    .then((dbUserData) => {
+      
+      req.session.save(() => {
+        
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json(dbUserData);
+      });
+    })
+    
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+router.post("/login", (req, res) => {
+  
+  User.findOne({
+    
+    where: {
+      
+      username: req.body.username,
+    },
+  }).then((dbUserData) => {
+    
+    if (!dbUserData) {
+      
+      res.status(400).json({ message: "USER NOT FOUND" });
+      
+      return;
+    }
+    
+    dbUserData.checkPassword(req.body.password).then((result) => {
+      
+      if (!result) {
+        
+        res.status(400).json({ message: "PASSWORD INCORRECT" });
+        
+        return;
+      }
+      
+      req.session.save(() => {
+        
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json({ user: dbUserData, message: "LOGGED IN" });
+      });
+    });
+  });
+});
+
+router.post("/logout", (req, res) => {
+  
+    if (req.session.loggedIn) {
+    
+        req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", withAuth, (req, res) => {
+  
+  User.update(req.body, {
+    
+    individualHooks: true,
+    where: {
+      
+      id: req.params.id,
+    },
+  })
+    
+    .then((dbUserData) => {
+      
+      if (!dbUserData[0]) {
+        
+        res.status(404).json({ message: "USER NOT FOUND" });
+        
+        return;
+      }
+      
+      res.json(dbUserData);
+    })
+    
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  
+  User.destroy({
+    
+    where: {
+      
+        id: req.params.id,
+    },
+  })
+    
+    .then((dbUserData) => {
+      
+      if (!dbUserData) {
+        
+        res.status(404).json({ message: "USER NOT FOUND" });
+       
+        return;
+      }
+      
+      res.json(dbUserData);
+    })
+    
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
