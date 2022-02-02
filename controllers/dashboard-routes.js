ac133d3f611a30d07a39ab8293a329b4c6c3ffeb
JonelleH <jonelle.harris04@gmail.com>
2022-02-02 18:31:42 -0500

    added code

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,101 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+  
+  Post.findAll({
+    
+    where: {
+      
+      user_id: req.session.user_id,
+    },
+   
+    attributes: ["id", "content", "title", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        order: [["created_at", "DESC"]],
+        include: {
+          
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    
+    .then((dbPostData) => {
+      
+      const posts = dbPostData.map((Post) => Post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  
+  Post.findOne({
+    
+    where: {
+      
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        order: [["created_at", "DESC"]],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    
+    .then((dbPostData) => {
+      
+      if (!dbPostData) {
+        
+        res.status(404).json({ message: "POST NOT FOUND " });
+        return;
+      }
+      
+      const post = dbPostData.get({ plain: true });
+      console.log("THIS IS POST: ", post);
+      res.render("edit-blog-post", {
+        
+        post,
+        loggedIn: true,
+      });
+    })
+    
+    .catch((err) => {
+      
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
